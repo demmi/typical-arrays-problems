b41b53332d850a2e3c9eb6c62c4fdb38ab3d23be
Andrew Gerasimchik <gerasimchick@tut.by>
2021-08-13 17:07:57 +0300

    init: create code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (arguments.length === 0) { 
+    return 0
+  }
+  let minArray = arguments[0];
+  for (let i of arguments) {
+    if (i < minArray) {
+      minArray = i;
+    }
+  }
+  return minArray;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (arguments.length === 0) { 
+    return 0
+  }
+  let maxArray = arguments[0];
+  for (let i of arguments) {
+    if (i > maxArray) {
+      maxArray = i;
+    }
+  }
+  return maxArray;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (arguments.length === 0) { 
+    return 0
+  }
+  let sumArray = arguments[0];
+  for (let i of arguments) {
+    sumArray += i;
+    
+  }
+  return sumArray / arguments.length;
 }
