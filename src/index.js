66ff708e81fda0f8c092473d74f3034c944af358
Andrew Gerasimchik <gerasimchick@tut.by>
2021-08-13 17:44:12 +0300

    fix: fix errors

--- src/index.js
+++ src/index.js
@@ -1,38 +1,78 @@
 
 exports.min = function min (array) {
-  if (arguments.length === 0) { 
-    return 0
-  }
-  let minArray = arguments[0];
-  for (let i of arguments) {
-    if (i < minArray) {
-      minArray = i;
+  if (typeof array === "number") {
+    if (arguments.length === 0) { 
+      return 0
+    }
+    let minArray = arguments[0];
+    for (let i of arguments) {
+      if (i < minArray) {
+        minArray = i;
+      }
+    }
+    return minArray;
+    }
+    else {
+    if (array.length === 0) { 
+      return 0
+    }
+    let minArray = array[0];
+    for (let i of array) {
+      if (i < minArray) {
+        minArray = i;
+      }
+    }
+    return minArray;
     }
   }
-  return minArray;
-}
+
 
 exports.max = function max (array) {
-  if (arguments.length === 0) { 
-    return 0
-  }
-  let maxArray = arguments[0];
-  for (let i of arguments) {
-    if (i > maxArray) {
-      maxArray = i;
+  if (typeof array === "number") {
+    if (arguments.length === 0) { 
+      return 0
+    }
+    let maxArray = arguments[0];
+    for (let i of arguments) {
+      if (i > maxArray) {
+        maxArray = i;
+      }
+    }
+    return maxArray;
+    }
+    else {
+    if (array.length === 0) { 
+      return 0
+    }
+    let minArray = array[0];
+    for (let i of array) {
+      if (i > minArray) {
+        minArray = i;
+      }
+    }
+    return minArray;
     }
   }
-  return maxArray;
-}
 
 exports.avg = function avg (array) {
-  if (arguments.length === 0) { 
-    return 0
+  if (typeof array === "number") {
+    if (arguments.length === 0) { 
+      return 0
+    }
+    let sumArray = arguments[0];
+    for (let i of arguments) {
+      sumArray += i;
+    }
+    return sumArray / arguments.length;
   }
-  let sumArray = arguments[0];
-  for (let i of arguments) {
-    sumArray += i;
-    
+  else {
+    if (array.length === 0) { 
+      return 0
+    }
+    let sumArray = array[0];
+    for (let i of array) {
+      sumArray += i;
+    }
+    return sumArray / array.length;
   }
-  return sumArray / arguments.length;
 }
